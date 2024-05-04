feb86a9118e359cf69530bd323a6078a8e31cd2f
pouria <pouria@cads.ai>
2024-05-04 16:16:08 +0330

    update answers

--- VCAREPlaftorm.Nuxt/pages/questions/answers.vue
+++ VCAREPlaftorm.Nuxt/pages/questions/answers.vue
@@ -4,35 +4,58 @@ definePageMeta({
 });
 
 // Composables
+const route = useRoute();
 const { GetQuestionsForDirectQuestions } = useQuestions();
 
 // States
 const loading = ref(true);
 const errMessage = ref();
+const allQuestions = ref("");
 
 // Logics
+const handleGetQuestionsForDirectQuestions = () => {
+  GetQuestionsForDirectQuestions(route.query.directId)
+    .then(async (r) => {
+      if (r) {
+        errMessage.value = null;
+        allQuestions.value = await r;
+      }
+    })
+    .catch((err) => (errMessage.value = err.data))
+    .finally(() => loading.value = false);
+}
 
 // select route query
 if (route.query.bodyPartId) {
-  handleGetBodyPartsForMainSymptom();
+  // handleGetBodyPartsForMainSymptom();
 } else if (route.query.symptomSubgroupId) {
-  handleGetSymptomSubgroupForMainSymptom();
+  // handleGetSymptomSubgroupForMainSymptom();
 } else if (route.query.directId) {
-  // GetQuestionsForDirectQuestions
+  handleGetQuestionsForDirectQuestions();
 }
 
 </script>
 <template>
-  <div class="pb-[140px]">
+  <div class="pb-[100px]">
 
     <Head>
       <Title>Questions</Title>
     </Head>
 
-    <div class="shadow-md bg-white text-[17px] text-black rounded-custom-10 mx-4 px-2 py-2 mt-4">
+    <div>
       <SharedLoading v-if="loading" class="mb-6" />
-      <div v-if="!loading" class="p-4">
-        11
+      <div v-if="!loading">
+        <div v-for="(item, index) in allQuestions" :key="index"
+          class="shadow-md bg-white text-[17px] text-black rounded-custom-10 mx-4 px-2 py-2 mt-4">
+          <div class="p-4">
+            {{ item.question.title }}
+            <div v-for="(questionItem, index) in item.question.questionOptions" :key="index" class="mt-4">
+              <input type="radio" name="question" :id="'question-' + questionItem.id" :value="questionItem.id"
+                class="mr-2" />
+              <label :for="'question-' + questionItem.id">{{ questionItem.title }}</label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
