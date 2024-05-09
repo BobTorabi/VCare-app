018b598825b0d3a8964a17e8a64f051b984d1e11
pouriakad <pouria@cads.ai>
2024-05-09 19:32:05 +0330

    multi select

--- VCAREPlaftorm.Nuxt/pages/questions/subGroup.vue
+++ VCAREPlaftorm.Nuxt/pages/questions/subGroup.vue
@@ -12,6 +12,8 @@ const loading = ref(true);
 const errMessage = ref();
 const AllBodyParts = ref('');
 const AllSymptom = ref('');
+const items = ref([]);
+const activeItems = ref([]);
 
 // Logics
 // Get Body Parts For Main Symptom
@@ -40,6 +42,23 @@ const handleGetSymptomSubgroupForMainSymptom = () => {
     .finally(() => loading.value = false);
 }
 
+// click On Items
+const clickOnItems = (id) => {
+  if (activeItems.value.includes(id)) {
+    const index = activeItems.value.indexOf(id);
+    if (index !== -1) {
+      activeItems.value.splice(index, 1);
+    }
+    const index2 = items.value.indexOf(id);
+    if (index2 !== -1) {
+      items.value.splice(index2, 1);
+    }
+  } else {
+    activeItems.value.push(id);
+    items.value.push(id);
+  }
+}
+
 // select route query
 if (route.query.bodyPart) {
   handleGetBodyPartsForMainSymptom();
@@ -60,11 +79,17 @@ if (route.query.bodyPart) {
       <div v-if="!loading" class="p-4">
         <div v-if="AllBodyParts">
           Please select the body part:
-          <div v-for="(item, index) in AllBodyParts" :key="index" class="main-category">
-            <NuxtLink :to="'/questions/answers?bodyPartId=' + item.id"
-              class="block text-center items-center font-bold bg-color-sec text-white rounded-custom-10 py-6 px-2 mt-4">
+          <div class="grid grid-cols-2 gap-4 mt-4 ">
+            <div v-for="(item, index) in AllBodyParts" :key="index"
+              class="block text-center place-content-center font-bold border border-color-pri rounded-custom-10 py-6 px-2 min-h-[90px] cursor-pointer"
+              @click="clickOnItems(item.id)" :class="{ 'active': activeItems.includes(item.id) }">
               {{ item.title }}
-            </NuxtLink>
+            </div>
+          </div>
+          <div class="">
+            <div class="bg-color-pri text-white w-full rounded-lg text-center p-3 pb-4 mt-4 block cursor-pointer">
+              Continue
+            </div>
           </div>
         </div>
         <div v-else-if="AllSymptom">
@@ -81,3 +106,9 @@ if (route.query.bodyPart) {
 
   </div>
 </template>
+<style>
+.active {
+  background: #2DB284;
+  color: #fff;
+}
+</style>
